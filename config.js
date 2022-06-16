ea85008d8578ca4f9d8b6034d4c92cb28631bf97
Fuad1877 <107192490+Fuad1877@users.noreply.github.com>
2022-06-16 14:14:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    app: {
-        px: 'XXX',
-        token: 'XXX',
+    app: {984417036639801354
+        px: 'fd88e28f1d631f8e045638489df19ce70270b64aa40b903589c422be384addce',
+        token: 'OTg0NDE3MDM2NjM5ODAxMzU0.GXBkRz._RG8ywSliZQnTkEwrnfSOnkbzLkS-WgGBxGBP8',
         playing: 'by Zerio ❤️'
     },
 
